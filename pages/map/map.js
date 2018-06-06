a0ff66d138980a253877aba9cb1a1b9474e8731f
weizhen <a610428@qq.com>
2018-06-07 07:29:35 +0800

    f

--- pages/map/map.js
+++ pages/map/map.js
@@ -5,13 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
+    latitude: 23.099994,
+    longitude: 113.324520,
     markers: [{
-      iconPath: "/resources/others.png",
-      id: 0,
+      iconPath: "/resources/location.png",
+      id: 1,
       latitude: 23.099994,
       longitude: 113.324520,
-      width: 50,
-      height: 50
+      name: 'T.I.T 创意园'
     }],
     polyline: [{
       points: [{
@@ -25,31 +26,49 @@ Page({
       width: 2,
       dottedLine: true
     }],
-    controls: [{
-      id: 1,
-      iconPath: '/resources/location.png',
-      position: {
-        left: 0,
-        top: 300 - 50,
-        width: 50,
-        height: 50
+  },
+
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function (res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
+  },
+
+  translateMarker: function () {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.10229,
+        longitude: 113.3345211,
       },
-      clickable: true
-    }]
-  },
-
-  regionchange(e) {
-    console.log(e.type)
-  },
-
-  markertap(e) {
-    console.log(e.markerId)
-  },
-
-  controltap(e) {
-    console.log(e.controlId)
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
   },
   
+  includePoints: function () {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      }, {
+        latitude: 23.00229,
+        longitude: 113.3345211,
+      }]
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -61,7 +80,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    this.mapCtx = wx.createMapContext('myMap')
+    // this.mapCtx.moveToLocation()
+
   },
 
   /**
